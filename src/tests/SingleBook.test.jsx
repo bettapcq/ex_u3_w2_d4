a1ac7be6bba84d60d1615cbe8bcc5ca8ac0a9c6e
Elisabetta Piacquadio <betta.pcq@gmail.com>
2025-11-27 16:46:07 +0100

    update

--- src/tests/SingleBook.test.jsx
+++ src/tests/SingleBook.test.jsx
@@ -5,7 +5,21 @@ import SingleBook from '../components/SingleBook';
 describe('Testing color card change', () => {
   it('Checks if Cards change color on click', async () => {
     // !)
-    render(<SingleBook />);
+    render(
+      <SingleBook
+        book={{
+          asin: '0316405345',
+          category: 'horror',
+          img: 'https://images-na.ssl-images-amazon.com/images/I/61rnkCg2iYL.jpg',
+          price: 25.48,
+          title: 'Invisible'
+        }}
+        asinFromBookList="0316405345"
+        changeSelectionState={changeSelectionState(false, '0316405345')}
+        selectedAsin="0316405345"
+        isSelected={false}
+      />
+    );
     // 2)
     const card = screen.getByTestId('border-card');
     expect(card).toHaveClass('');
